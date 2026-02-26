e2cb94eb71dcc94d57c684e5ab8d9e19947c3f7b
HSingh2004 <127125215+HSingh2004@users.noreply.github.com>
2026-02-26 00:21:06 -0600

    Update job search results per page to 7

--- api/jobs.js
+++ api/jobs.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      `https://api.adzuna.com/v1/api/jobs/ca/search/1?app_id=${process.env.ADZUNA_ID}&app_key=${process.env.ADZUNA_KEY}&results_per_page=20&what=cybersecurity&max_days_old=30`
+      `https://api.adzuna.com/v1/api/jobs/ca/search/1?app_id=${process.env.ADZUNA_ID}&app_key=${process.env.ADZUNA_KEY}&results_per_page=7&what=cybersecurity&max_days_old=30`
     );
 
     const data = await response.json();
